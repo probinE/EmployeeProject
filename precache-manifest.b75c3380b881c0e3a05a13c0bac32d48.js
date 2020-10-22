e188f545e31e2fc6cc62fcb198dfd06653bf4074
probinE <e.probin49@gmail.com>
2020-10-22 12:24:17 +0530

    Updates

---
+++ precache-manifest.b75c3380b881c0e3a05a13c0bac32d48.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95aa70629d57d6de63e98bad80715a75",
+    "url": "/probinE/EmployeeProject/index.html"
+  },
+  {
+    "revision": "481379c99b915bb301b9",
+    "url": "/probinE/EmployeeProject/static/css/2.fbfb6306.chunk.css"
+  },
+  {
+    "revision": "1733b633f4d4874e520d",
+    "url": "/probinE/EmployeeProject/static/css/main.b04b2e85.chunk.css"
+  },
+  {
+    "revision": "481379c99b915bb301b9",
+    "url": "/probinE/EmployeeProject/static/js/2.e7bf4395.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/probinE/EmployeeProject/static/js/2.e7bf4395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1733b633f4d4874e520d",
+    "url": "/probinE/EmployeeProject/static/js/main.ccdbbebe.chunk.js"
+  },
+  {
+    "revision": "d9a343159972c73892e6",
+    "url": "/probinE/EmployeeProject/static/js/runtime-main.57194408.js"
+  },
+  {
+    "revision": "965a7a1a7bd29d1565b5edeca7b4b9b7",
+    "url": "/probinE/EmployeeProject/static/media/icons_mobiscroll.965a7a1a.woff"
+  },
+  {
+    "revision": "c405c13ebacaadfc5def2aa6f9ae7b7d",
+    "url": "/probinE/EmployeeProject/static/media/icons_mobiscroll.c405c13e.ttf"
+  }
+]);
